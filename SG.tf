9f807d3b2d96c5dd34b8b5be5ca7d2c76ec852e4
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-05 15:48:40 +0530

    Update SG.tf

--- SG.tf
+++ SG.tf
@@ -3,7 +3,7 @@
 resource "aws_security_group" "wp-sg-tf" {
   name        = "wp-instance-tf"
   description = "Security group for EC2 Instances"
-  vpc_id      = "var.aws_vpc.default.id"
+  vpc_id      = var.aws_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -34,7 +34,7 @@ resource "aws_security_group" "wp-sg-tf" {
 resource "aws_security_group" "wp-db-sg-tf" {
   name        = "wp-db-tf"
   description = "Access to the RDS instances from the VPC"
-  vpc_id      = "var.aws_vpc.default.id"
+  vpc_id      = var.aws_vpc.default.id
 
   ingress {
     from_port   = 3306
@@ -65,7 +65,7 @@ resource "aws_security_group" "wp-db-sg-tf" {
 resource "aws_security_group" "wp-elb-tf" {
   name        = "wp-sg-elb-tf"
   description = "Security Group for the ELB"
-  vpc_id      = "var.aws_vpc.default.id"
+  vpc_id      = var.aws_vpc.default.id
 
   ingress {
     from_port   = 80
