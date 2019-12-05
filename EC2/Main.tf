e34b3a7ba8d473a0d69372b3cc553c3c7cc46757
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-05 15:40:28 +0530

    Update Main.tf

--- EC2/Main.tf
+++ EC2/Main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2-instance" {
     subnet_id                   = var.aws_subnet.wp-public-tf.id
     key_name                    = var.key_name
     associate_public_ip_address = true
-    security_groups             = [aws_security_group.wp-sg-tf.id]
+    security_groups             = [var.aws_security_group.wp-sg-tf.id]
     user_data                   = file("EC2/installing-components.sh")
     tags = {
       Name = "ec2-instance"
